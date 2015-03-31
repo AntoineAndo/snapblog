99a9fcf9c0f1f7e73583e9e4f28d02cd46eaf14f
AntoineAndo <antoine.ando@gmail.com>
2015-03-31 21:02:50 +0200

    Insertion MySQL
    Problème de délai

---
+++ DAO/connection.js
@@ -0,0 +1,42 @@
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'snapblog'
+});
+
+var insertPost = function (title,content,checkDate,expDate,expCountdown,callback){  
+		var date = new Date();
+		console.log(date);
+
+	if(checkDate == 'date')
+	{
+		var pattern = /(\d{2})\/(\d{2})\/(\d{4})/;
+		expDate = new Date(expDate.replace(pattern,'$3-$2-$1'));
+		expDate.setHours(0);
+		expDate.setMinutes(0);
+		expDate.setSeconds(0);
+	}
+	else if (checkDate == 'duration')
+	{
+		date.setHours(parseInt(date.getHours())+parseInt(expCountdown));
+    	expDate = date;
+	}
+
+	expDate = expDate.getFullYear() + "-" + (expDate.getMonth()+1) + "-" + expDate.getDate()+ " " +expDate.getHours() + ":" + expDate.getMinutes() + ":" + expDate.getSeconds();
+
+	console.log(title, content, checkDate, expDate, expCountdown);
+
+	query = connection.query('INSERT INTO articles (title, content, expirationDate) VALUES ("'+title+'","'+content+'","'+expDate+'")'), function res(err, rows, field) {
+		console.log('test');	
+
+    callback(rows[0]);
+		console.log('test');
+    
+  };
+  
+};
+
+exports.insertPost = insertPost;
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var session = require('cookie-session');
+var bodyParser = require('body-parser');
+var reqmysql = require("./DAO/connection");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var app = express();
+
+app.use(session({secret: 'todotopsecret'}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json())
+
+
+.get('/', function(req, res) { 
+	msg="";
+    res.render('index.ejs', {message: msg});
+})
+
+.post('/post', function(req, res){
+	var title = req.body.titleArticle;
+	var content = req.body.content;
+	var checkDate = req.body.checkDate;
+	var expDate = req.body.expirationDate;
+	var expCountdown = req.body.expirationCountdown;
+
+	reqmysql.insertPost(title,content,checkDate,expDate,expCountdown, function callback (result){
+		msg="Votre article a été posté";
+		console.log('test');
+		res.render('index.ejs'), {message: msg};
+
+	});
+})
+
+.use(express.static(__dirname + '/public'))
+
+.listen(8080);
+
